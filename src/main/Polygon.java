ad2544bed6577b31eb4a6d985521a473fb9f204a
Matt Mortimer <matthew.mortimer1@gmail.com>
2013-08-14 14:06:08 +1200

    initial commit

---
+++ src/main/Polygon.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Polygon {
+	private Vertex v1;
+	private Vertex v2;
+	private Vertex v3;
+	private int r;
+	private int g;
+	private int b;
+
+	public Polygon(Vertex v1, Vertex v2, Vertex v3, int r, int g, int b){
+
+	}
+}
---
+++ src/main/Vertex.java
@@ -0,0 +1,39 @@
+package main;
+
+public class Vertex {
+	private double x;
+	private double y;
+	private double z;
+
+	public Vertex(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public void setX(double x){
+		this.x = x;
+	}
+
+	public double getX(){
+		return this.x;
+	}
+
+	public void setY(double y){
+		this.y = y;
+	}
+
+	public double getY(){
+		return this.x;
+	}
+
+	public void setZ(double z){
+		this.z = z;
+	}
+
+	public double getZ(){
+		return this.x;
+	}
+
+
+}
