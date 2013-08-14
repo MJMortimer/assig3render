c9c5f6eeaaee9aa9ba4decd589b7795517d0d918
Matt Mortimer <matthew.mortimer1@gmail.com>
2013-08-14 14:28:22 +1200

    Working on JFrame

---
+++ src/main/RenderFrame.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.awt.Canvas;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+
+public class RenderFrame extends JFrame {
+	private JMenuBar menuBar;
+	private JComponent container;
+	private Canvas drawing;
+
+	public RenderFrame(){
+		super("3D Renderer");
+		init();
+	}
+
+	public void init(){
+
+	}
+}
